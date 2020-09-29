224bec07da8a8e4885ca248f72a3bfd747d76f24
KylevanderWolf <68204507+KylevanderWolf@users.noreply.github.com>
2020-09-29 11:02:42 +0200

    If-Else-Statement

---
+++ Javascript_3/script.js
@@ -0,0 +1,29 @@
+//Opdracht1
+const age = 18
+
+if (age >= 18) {
+    console.log("You can join the club!");
+}
+else {
+    console.log("Get out of here! Kid");
+}
+
+//Opdracht2
+const isFemale = true
+
+if (isFemale) {
+    console.log("You can join the Ladies Night!");
+}
+else {
+    console.log("Get out of here!");
+}
+
+//Opdracht3
+const driverStatus = "bob"
+
+if (driverStatus == "bob") {
+    console.log("You can drive home safely");
+}
+else {
+    console.log("Get an uber, you are too drunk");
+}
